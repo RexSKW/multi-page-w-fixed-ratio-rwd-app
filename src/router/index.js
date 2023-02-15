cdc739176b92ed7182d60f70568d9fd61a54f8fa
rex <rex.swijo@gmail.com>
2023-02-15 13:56:37 +0800

    chore: 新增jsconfig.json來實現alias檔案連結的功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import LoginPageView from '../views/LoginPageView/LoginPageView.vue';
+import LoginPageView from '@/views/LoginPageView/LoginPageView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
